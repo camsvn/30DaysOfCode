5fc6236d39b733b8f284f9525b01d1d66d026988
Amal Salvin <amalsalvin97@gmail.com>
2020-10-29 18:52:04 +0530

    Week2 Wednesday

---
+++ 29102020/websocket/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const WebSocket = require('ws');
+const SocketServer = require('ws').Server;
+
+const server = express().listen(3000, () => {
+    console.log('Server started at 3000..');
+});
+
+const wss = new SocketServer({ server });
+
+wss.on('connection', function connection(ws) {
+    ws.on('message', function incoming(data) {
+      wss.clients.forEach(function each(client) {
+        if (client !== ws && client.readyState === WebSocket.OPEN) {
+          client.send(data);
+        }
+      })
+    })
+  })
