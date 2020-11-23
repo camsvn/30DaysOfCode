83568bcfa97b351dfae157b327f2a1695854c3b3
Amal Salvin <amalsalvin97@gmail.com>
2020-11-23 12:36:46 +0530

    Angular Routing

--- 13112020/Angular/learn-angular/src/app/app.module.ts
+++ 13112020/Angular/learn-angular/src/app/app.module.ts
@@ -1,13 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router'
 
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
 import { HighlightDirective } from './directives/highlight.directive';
 
-import { DepInjModule } from './dep-inj/dep-inj.module'
+import { DepInjModule } from './dep-inj/dep-inj.module';
+import { DepInjComponent } from './dep-inj/dep-inj.component';
+import { PageNotFound404Component } from './page-not-found404/page-not-found404.component'
 
 @NgModule({
   declarations: [
@@ -15,11 +18,18 @@ import { DepInjModule } from './dep-inj/dep-inj.module'
     ServerComponent,
     ServersComponent,
     HighlightDirective,
+    PageNotFound404Component,
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    DepInjModule
+    DepInjModule, //feature module
+    RouterModule.forRoot([
+      {path: '', redirectTo:'/angular-basics', pathMatch:'full'},
+      {path: 'angular-basics', component: ServersComponent},
+      {path: 'directives', component: DepInjComponent},
+      {path: '**', component: PageNotFound404Component}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
