2bead17956777d297d626d6daee38fbc695e0f37
Amal Salvin <amalsalvin97@gmail.com>
2020-11-24 14:55:44 +0530

    RxJs Subjects

--- 03112020/RxJS/src/components/subjects.ts
+++ 03112020/RxJS/src/components/subjects.ts
@@ -1,4 +1,4 @@
-import {from, Subject, interval} from 'rxjs'
+import {from, Subject, interval, BehaviorSubject, ReplaySubject, AsyncSubject} from 'rxjs'
 import {multicast, refCount, take} from 'rxjs/operators'
 import {fontSize, sleep, addItem} from './helpers'
 
@@ -33,28 +33,106 @@ export function subjects (){
     
     // This calls `connect()`, because
     // it is the first subscriber to `refCounted`
-    console.log('observerA subscribed');
     subscription1 = refCounted.subscribe({
     next: (v) => addItem(v,5,'Observer C')
     });
     
     setTimeout(() => {
-    console.log('observerB subscribed');
     subscription2 = refCounted.subscribe({
-        next: (v) => console.log(v,5,'Observer D')
+        next: (v) => addItem(v,5,'Observer D')
     });
     }, 600);
     
     setTimeout(() => {
-    console.log('observerA unsubscribed');
+    addItem("Observer C",5,'unsubscribed')
     subscription1.unsubscribe();
     }, 1200);
     
     // This is when the shared Observable execution will stop, because
     // `refCounted` would have no more subscribers after this
     setTimeout(() => {
-    console.log('observerB unsubscribed');
+    addItem("Observer D",5,'unsubscribed')
     subscription2.unsubscribe();
     }, 2000);
+
+/**
+ * 3 variants of subject:
+ *      ● Behavior Subject - It stores the latest value emitted to its consumers,
+ *                              and whenever a new Observer subscribes,
+ *                              it will immediately receive the "current value"
+ *      
+ *      ● Replay Subject - It can send old values to new subscribers,
+ *                              but it can also record a part of the Observable execution.
+ *                         ie,
+ *                              records multiple values from the Observable execution and replays them to new subscribers.
+ * 
+ *      ● Async Subject - only the last value of the Observable execution is sent to its observers,
+ *                              and only when the execution completes.
+ */
+    sleep(6002).then(()=>{
+//Behavior Subject
+        addItem('BehaviorSubjects',5,'',fontSize.h2)
+
+        const behaviorSubject = new BehaviorSubject(0);
+
+        behaviorSubject.subscribe((v) => addItem(v,5,'Observer E'))
+
+        behaviorSubject.next(1);
+        behaviorSubject.next(2);
+
+        behaviorSubject.subscribe((v) => addItem(v,5,'Observer F - receives latest value'))
+
+//Replay Subject
+        addItem('ReplaySubjects',5,'',fontSize.h2)
+        const replaySubject = new ReplaySubject(3);
+
+        replaySubject.subscribe({
+            next: (v) => addItem(v,5,'Observer G')
+          });
+           
+        replaySubject.next(1);
+        replaySubject.next(2);
+        replaySubject.next(3);
+        replaySubject.next(4);
+        
+          replaySubject.subscribe({
+            next: (v) => addItem(v,5,'Observer H - buffers 3 values')
+        });
+        
+        replaySubject.next(5);  
+
+//Replay Subject with windowTime
+        addItem('ReplaySubjects',5,'with windowTime',fontSize.h3)
+        const repWTSubject = new ReplaySubject(10,500);
+        
+        repWTSubject.subscribe(v => addItem(v,5,'Observer I'))
+
+        const counter = interval(200).pipe(take(9));
+        counter.subscribe(v => {
+            repWTSubject.next(v)
+        })
+
+        setTimeout(() => {
+            repWTSubject.subscribe(v => addItem(v,5,'Observer J'))
+        },1500)
+
+//AsyncSubject 
+        sleep(2250).then(()=>{
+            addItem('AsyncSubjects',5,'only the last value of the Observable execution is sent',fontSize.h2)
+                const asyncSubjects = new AsyncSubject();
+
+                asyncSubjects.subscribe(v => addItem(v,5,'Observer K'))
+
+                asyncSubjects.next(1)
+                asyncSubjects.next(2)
+                asyncSubjects.next(3)
+                asyncSubjects.next(4)
+
+                asyncSubjects.subscribe(v => addItem(v,5,'Observer L'))
+
+                asyncSubjects.next(5)
+                asyncSubjects.complete()
+        })
+    })   
 
 }
