115708f41b52cfe49cfa887e80f5a29c03b1d882
Amal Salvin <amalsalvin97@gmail.com>
2020-11-30 16:06:46 +0530

    DB: Table created

--- 26112020/ionic/learn-ionic/src/app/home/home.page.ts
+++ 26112020/ionic/learn-ionic/src/app/home/home.page.ts
@@ -9,15 +9,30 @@ import { SQLiteService } from '../services/sqlite.service'
 })
 export class HomePage implements AfterViewInit {
 
-  messages : string [] = ["Start", "1"];
+  messages : string [] = ["Start", "End"];
+
+  createTBLQuery =`CREATE TABLE IF NOT EXISTS "user" (
+    "id" INTEGER PRIMARY KEY AUTOINCREMENT,
+    "fname" VARCHAR(30) NOT NULL,
+    "lname" VARCHAR(30) NOT NULL,
+    "username" VARCHAR(30) NOT NULL UNIQUE,
+    "password" VARCHAR(30) NOT NULL
+    );
+    `; 
 
   constructor(private _SQLiteService: SQLiteService) {}
 
   ngAfterViewInit(){
     this._SQLiteService.initializePlugin()
-    this.openDB(); 
     setTimeout(()=> {
       this.openDB()
+      this._SQLiteService.execute(this.createTBLQuery).then(data => {
+        console.log("Returned",data)
+        if(data.changes.changes === 0)
+          console.log("Table 'user' created")
+        else 
+          console.log("Table not created", data.message)
+      });
     }, 3000)
   }
 
