21c4e65b4bd7e60198f91c77826f4ffb84867068
Amal Salvin <amalsalvin97@gmail.com>
2020-11-30 07:00:05 +0530

    Sqlite Plugin Added Added Plugin Created a dbService

--- 26112020/ionic/learn-ionic/src/app/home/home.page.ts
+++ 26112020/ionic/learn-ionic/src/app/home/home.page.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import { SQLiteService } from '../services/sqlite.service'
+
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements AfterViewInit {
 
-  constructor() {}
+  messages : string [] = ["Start", "1"];
 
+  constructor(private _SQLiteService: SQLiteService) {}
+
+  ngAfterViewInit(){
+    this._SQLiteService.initializePlugin()
+    this.openDB(); 
+    setTimeout(()=> {
+      this.openDB()
+    }, 3000)
+  }
+
+  async openDB(): Promise<void> {
+    if(this._SQLiteService.isService) {
+      let resOpen = await this._SQLiteService.openDB("bizRuntimeDB"); 
+      if(resOpen.result) {
+       console.log("DB Created")
+      }
+    } else {
+      console.log('CapacitorSQLite Plugin: Initialization Failed');
+    }
+}
 }
