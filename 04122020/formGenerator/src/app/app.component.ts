011b2b747d1e4012d24dd33c0c477a4c63810c1e
Amal Salvin <amalsalvin97@gmail.com>
2020-12-04 17:35:04 +0530

    FormGenerator: Material Added

--- 04122020/formGenerator/src/app/app.component.ts
+++ 04122020/formGenerator/src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'formGenerator';
+  exampleJsonObject = {
+    "first_name": "Jane", "last_name": "Doe", "age": 25, "is_company": false,
+    "address": {
+      "street_1": "123 Main St.", "street_2": null,
+      "city": "Las Vegas", "state": "NV", "zip_code": "89123"
+    },
+    "phone_numbers": [
+      { "number": "702-123-4567", "type": "cell" },
+      { "number": "702-987-6543", "type": "work" }
+    ], "notes": ""
+  };
+
+  submitData: any;
+
+  submit(data:any){
+    console.log(data)
+  }
 }
--- 04122020/formGenerator/src/app/app.module.ts
+++ 04122020/formGenerator/src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+// import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { JsonSchemaFormModule } from '@ajsf/core';
+import { MaterialDesignFrameworkModule } from '@ajsf/material';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    // FormsModule,
+    JsonSchemaFormModule,
+    MaterialDesignFrameworkModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
